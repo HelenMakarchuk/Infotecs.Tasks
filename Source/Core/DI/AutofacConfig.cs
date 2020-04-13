a307c0625acf68ca46c39438a7a0f63c52d58f81
Helena Makarchuk <helenamakarchuk@gmail.com>
2020-04-13 11:03:24 +0300

    Add Autofac

---
+++ Source/Core/DI/AutofacConfig.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using System;
+
+namespace Core.DI
+{
+    public static class AutofacConfig
+    {
+        public static IContainer Configure(params Module[] modules)
+        {
+            var builder = new ContainerBuilder();
+            Array.ForEach(modules, m => builder.RegisterModule(m));
+            return builder.Build();
+        }
+    }
+}
