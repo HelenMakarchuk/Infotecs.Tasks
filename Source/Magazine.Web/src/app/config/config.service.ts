65189364ed0a7f02545a28febd6dfb791fca6537
Helena Makarchuk <helenamakarchuk@gmail.com>
2020-05-19 12:18:23 +0300

    Update Web

--- Source/Magazine.Web/src/app/config/config.service.ts
+++ Source/Magazine.Web/src/app/config/config.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-/*import { Observable, throwError } from 'rxjs'*/;
-//import { catchError, retry } from 'rxjs/operators';
-//import { Config } from './config';
 import { environment } from '../../environments/environment';
 import { IConfig } from '../models/IConfig';
 
