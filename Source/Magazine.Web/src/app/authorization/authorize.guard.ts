c43232c0f0b075f7e1151335ba1d7a9983491634
Helena Makarchuk <helenamakarchuk@gmail.com>
2020-06-09 10:56:56 +0300

    Apply formatting

--- Source/Magazine.Web/src/app/authorization/authorize.guard.ts
+++ Source/Magazine.Web/src/app/authorization/authorize.guard.ts
@@ -6,29 +6,29 @@ import { tap } from 'rxjs/operators';
 import { ApplicationPaths, QueryParameterNames } from './authorization.constants';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthorizeGuard implements CanActivate {
-  constructor(private authorize: AuthorizeService,
-    private router: Router) { }
+    constructor(private authorize: AuthorizeService,
+        private router: Router) { }
 
-  canActivate(_next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    
-    return this.authorize.isAuthenticated()
-      .pipe(tap(isAuthenticated => {
-        
-        return this.handleAuthorization(isAuthenticated, state);
-      }));
-  }
+    canActivate(_next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-  private handleAuthorization(isAuthenticated: boolean, state: RouterStateSnapshot) {
-    
-    if (!isAuthenticated) {
-      this.router.navigate(ApplicationPaths.LoginPathComponents, {
-        queryParams: {
-          [QueryParameterNames.ReturnUrl]: state.url
-        }
-      });
+        return this.authorize.isAuthenticated()
+            .pipe(tap(isAuthenticated => {
+
+                return this.handleAuthorization(isAuthenticated, state);
+            }));
     }
-  }
+
+    private handleAuthorization(isAuthenticated: boolean, state: RouterStateSnapshot) {
+
+        if (!isAuthenticated) {
+            this.router.navigate(ApplicationPaths.LoginPathComponents, {
+                queryParams: {
+                    [QueryParameterNames.ReturnUrl]: state.url
+                }
+            });
+        }
+    }
 }
