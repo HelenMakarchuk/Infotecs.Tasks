ed2a6897debf5ef7539918825d401fa6be434e4e
Helena Makarchuk <helenamakarchuk@gmail.com>
2020-06-15 22:37:52 +0300

    Update

--- Source/Magazine.Web/src/environments/environment.development.ts
+++ Source/Magazine.Web/src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:5080/api',
     hubUrl: 'http://localhost:5080',
-    identityUrl: 'http://localhost:5082',
+    identityUrl: 'https://localhost:5082',
     baseUrl: 'http://localhost:4200'
 };
