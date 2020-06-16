27679a724206f21e004062923c357fde4f54ed90
Helena Makarchuk <helenamakarchuk@gmail.com>
2020-06-16 13:46:07 +0300

    Update

--- Source/Magazine.Web/src/environments/environment.production.ts
+++ Source/Magazine.Web/src/environments/environment.production.ts
@@ -3,5 +3,5 @@ export const environment = {
     apiUrl: '/api',
     hubUrl: '/',
     identityUrl: 'https://localhost:5082',
-    baseUrl: 'http://localhost:5498'
+    baseUrl: 'https://localhost:5498'
 };
